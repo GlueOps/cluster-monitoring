9b12b7c70d305eac37be64c29d2a06f81809043c
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-09-04 14:32:52 +0000

    feat: add pinging rootly heartbeat

--- monitoring_script.py
+++ monitoring_script.py
@@ -13,7 +13,7 @@ logger = go_configure_logging(
 
 def is_cluster_healthy(config):
     return (
-        get_alertmanager_notifification_health_for_opsgenie(config.PROMETHEUS_QUERY_URL) and
+        get_alertmanager_notifification_health_for_rootly(config.PROMETHEUS_QUERY_URL) and
         check_for_200_response(config.PROMETHEUS_URL_HEALTH) and
         check_for_200_response(config.PROMETHEUS_URL_READY) and
         check_for_200_response(config.ALERTMANAGER_URL_HEALTH) and
@@ -72,12 +72,49 @@ def get_alertmanager_notifification_health_for_opsgenie(prometheus_query_url):
         logger.exception(f"Error querying Prometheus: {e}")
         raise
     
-def send_opsgenie_heartbeat(config):
-    url = f"https://api.opsgenie.com/v2/heartbeats/{config.OPSGENIE_HEARTBEAT_NAME}/ping"
-    headers = {
-        "Authorization": f"GenieKey {config.OPSGENIE_API_KEY}"
+
+def get_alertmanager_notifification_health_for_rootly(prometheus_query_url):
+    # Prometheus query
+    query = 'sum(increase(alertmanager_notifications_failed_total{integration="webhook"}[10m]))'
+
+    # Parameters for the request
+    params = {
+        'query': query
     }
 
+    try:
+        # Send the request to Prometheus
+        response = requests.get(prometheus_query_url, params=params)
+        response.raise_for_status()
+
+        # Parse the JSON response
+        result = response.json()
+
+        # Check the status of the response
+        if result['status'] == 'success':
+            # Extract the result
+            data = result.get('data', {})
+            result_type = data.get('resultType', '')
+
+            # Check if there are any results
+            if result_type == 'vector' and data.get('result', []):
+                # Get the value of the result
+                value = float(data['result'][0]['value'][1])
+
+                # Return True if the value is 0, False otherwise
+                return value == 0
+
+        return False
+
+    except requests.RequestException as e:
+        logger.exception(f"Error querying Prometheus: {e}")
+        raise
+
+def send_opsgenie_heartbeat(config):
+    url = f"https://api.opsgenie.com/v2/heartbeats/{config.HEARTBEAT_NAME}/ping"
+    headers = {
+        "Authorization": f"GenieKey {config.HEARTBEAT_API_KEY}"
+    }
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
@@ -86,15 +123,31 @@ def send_opsgenie_heartbeat(config):
     except requests.RequestException as e:
         logger.exception(f"Failed to send Opsgenie heartbeat. Error: {e}")
         raise
-    
+
+def send_rootly_heartbeat():
+    url = 'https://api.rootly.com/v1/heartbeats/ping'
+    headers = {
+        "Authorization": f"Bearer {config.HEARTBEAT_API_KEY}"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        logger.debug("Pinged Rootly heartbeat successfully!")
+
+    except requests.RequestException as e:
+        logger.exception(f"Failed to send Rootly heartbeat. Error: {e}")
+        raise
+
+
+
 if __name__ == '__main__':
     config = ServiceConfig()   
-    interval_in_seconds = config.OPSGENIE_PING_INTERVAL_MINUTES * 60
+    interval_in_seconds = config.PING_INTERVAL_MINUTES * 60
 
     # Check if the interval is less than 1 minute
     if interval_in_seconds < 60:
         try:
-            raise ValueError("OPSGENIE_PING_INTERVAL_MINUTES must be 1 minute or greater.")
+            raise ValueError("PING_INTERVAL_MINUTES must be 1 minute or greater.")
         except Exception as e:
             logger.exception(str(e))
             raise
@@ -108,10 +161,10 @@ if __name__ == '__main__':
         if execution_count < 2:
             if is_cluster_healthy(config):
                 logger.info("Checks: STARTED")
-                send_opsgenie_heartbeat(config)
+                send_rootly_heartbeat(config)
                 logger.info("Checks: PASSED")
             else:
-                logger.error(f"One or more health checks failed. Heartbeat for {config.OPSGENIE_HEARTBEAT_NAME} was not sent")   
+                logger.error(f"One or more health checks failed. Heartbeat for {config.HEARTBEAT_NAME} was not sent")   
                 logger.info(f"Waiting {interval_in_seconds+120} seconds before checking again")
                 time.sleep(interval_in_seconds+120) 
             execution_count += 1
